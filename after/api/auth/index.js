ea4227580ec1112d176c2c06a0e88eddaff4f42a
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2022-05-18 02:25:58 +0900

    week5

---
+++ after/api/auth/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import login from "./login";
+import register from "./register";
+
+
+const router = express();
+
+router.use("/login", login);
+router.use("/register", register);
+
+export default router;
